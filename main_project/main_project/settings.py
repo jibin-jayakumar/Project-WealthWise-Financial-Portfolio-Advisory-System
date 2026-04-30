205decca91dd22a957702db86ea9c254cc51171b
jibin-jayakumar <jibinjayakumar001@gmail.com>
2026-04-30 20:25:29 +0530

    3rd commit

--- main_project/main_project/settings.py
+++ main_project/main_project/settings.py
@@ -50,7 +50,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddeleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -84,13 +86,9 @@ WSGI_APPLICATION = 'main_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'MAIN_PROJECT',
-        'USER':'root',
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST':'localhost',
-        'PORT':'3306'
-
+        'ENGINE': 'django.db.backends.sqlite',
+        'NAME': BASE_DIR/'db.sqlite3',
+        
     }
 }
 
@@ -130,7 +128,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR,'static')
 
+]
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+ALLOWED_HOSTS=['*']
+ 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
