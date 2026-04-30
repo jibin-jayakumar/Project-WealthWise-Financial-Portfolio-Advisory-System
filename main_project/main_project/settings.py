6055326bb395ed5e58130b26117447d50e94d3bc
jibin-jayakumar <jibinjayakumar001@gmail.com>
2026-04-30 20:44:21 +0530

    fix sqlite

--- main_project/main_project/settings.py
+++ main_project/main_project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'main_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR/'db.sqlite3',
         
     }
