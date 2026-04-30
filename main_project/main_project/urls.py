12b9c558c859bd3e62339daa3d3ac4d965c7ae7d
jibin-jayakumar <jibinjayakumar001@gmail.com>
2026-05-01 03:19:43 +0530

    Add catch-all route for React Router

--- main_project/main_project/urls.py
+++ main_project/main_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -23,5 +23,6 @@ urlpatterns = [
     path('api/',include('app_users.urls')),
     path('api/',include('app_portfolio.urls')),
     path('api/',include('app_advisors.urls')),
-    path('',TemplateView.as_view(template_name='index.html'))
+    # path('',TemplateView.as_view(template_name='index.html'))
+    re_path(r'^(?!api/).*$', TemplateView.as_view(template_name='index.html')),
 ]
