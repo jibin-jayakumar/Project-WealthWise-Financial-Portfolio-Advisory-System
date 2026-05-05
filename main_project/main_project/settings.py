292771eb24433d45f4aa9628ff6a32c00844bc0e
jibin-jayakumar <jibinjayakumar001@gmail.com>
2026-05-05 20:39:31 +0530

    latest debug

--- main_project/main_project/settings.py
+++ main_project/main_project/settings.py
@@ -83,13 +83,11 @@ WSGI_APPLICATION = 'main_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'db.sqlite3',
-        
-    }
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL")
+    )
 }
 
 
