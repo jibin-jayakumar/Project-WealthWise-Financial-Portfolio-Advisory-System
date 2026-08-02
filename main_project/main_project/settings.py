2b4a55b55383b3febe7ee0a1783731ddf084a5ac
jibin-jayakumar <jibinjayakumar001@gmail.com>
2026-08-02 21:43:49 +0530

    Fix use sqlite locally ,postgreSql on render

--- main_project/main_project/settings.py
+++ main_project/main_project/settings.py
@@ -82,12 +82,21 @@ WSGI_APPLICATION = 'main_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-import dj_database_url
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.getenv("DATABASE_URL")
-    )
-}
+
+if os.getenv('RENDER'):
+    # On Render - use PostgreSQL with internal host
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+    }
+else:
+    # Local development - use SQLite (simpler)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
